365103892f67c2cf8b9b13ea159580dce47b286b
Abhishek <abhigadekar34@gmail.com>
2025-09-03 21:53:18 +0530

    ready

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build", // default is "dist", change to "build"
-  },
+  base: "./",  // ✅ Important for Vercel deployment
 });
